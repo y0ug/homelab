7efa715869d3db3d346b4eeb55ad0976b801d51d
Hugo Caron <hca443@gmail.com>
2025-01-03 15:53:19 +0100

    add sops secrets for prod env

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
-sops -d terraform/terraform.tfvars.enc >terraform/terraform.tfvars
-sops -d .kube_config.yaml.enc >.kube_config.yaml
+ENV="${1:-prod}"
+export DEST=$(pwd)/.dec
+export SRC=$(pwd)/secrets/$ENV
+
+mkdir -p "$DEST"
+
+sops -d "$SRC"/kube_config.sops.yaml >"$DEST/kube_config.dec.yaml"
+sops -d "$SRC"/scw.sops.yaml >"$DEST/scw.dec.yaml"
+sops -d --output-type dotenv "$SRC/terraform.sops.yaml" >$DEST/terraform.dec.env
+
+export KUBE_CONFIG_PATH="$DEST/kube_config.dec.yaml"
+export SCW_CONFIG_PATH="$DEST/scw.dec.yaml"
+eval $(sops -d --output-type dotenv "$SRC/terraform.sops.yaml")
